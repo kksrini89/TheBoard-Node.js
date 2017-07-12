55c4f78b3a33c553948e0361a1975b22a56aa35b
srinivasan <kksrini89@gmail.com>
2017-07-12 22:18:23 +0530

    untracked file entry ntvs analysis dat file

--- TheBoard/data/index.js
+++ TheBoard/data/index.js
@@ -1,8 +1,37 @@
 ﻿(function (data) {
     var seedData = require('./seedData');
+    var database = require('./database');
     
     data.getNoteCategories = function (next) {
         next(null, seedData.initialNotes);
     }
+    
+    function seedDatabase() {
+        database.getDb(function (err, db) {
+            if (err) {
+                console.log("Failed to seed database: " + err);
+            } else {
+                db.notes.count(function (err, count) {
+                    if (err)
+                        console.log("Failed to retrieve database count");
+                    else {
+                        if (count === 0) {
+                            console.log("Seeding Database");
+                            seedData.initialNotes.forEach(function (item) {
+                                db.notes.insert(item, function (err) {
+                                    if (err)
+                                        console.log("Failed to insert note to database");
+                                });
+                            });
+                        } else {
+                            console.log("Already database seeded!");
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    seedDatabase();
 
 })(module.exports);
